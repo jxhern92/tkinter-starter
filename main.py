6ba2476c34c34ef8baa0fb1df16420cd58d65a7b
Javier <60404224+jxhern92@users.noreply.github.com>
2020-01-29 14:34:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,23 @@
 from tkinter import * # import Tkinter library
 window = Tk()         # Create the application window
 
+window.geometry('350x200')
+
 # Add a label with the text "Hello"
-lbl = Label(window, text="This is how it happened. This is how the batman died - Jim Gorden")
+lbl = Label(window, text="Hello", font=("Arial Bold", 50))
 
 # Place the label in the window at 0, 0
 lbl.grid(column=0, row=0)
 
+txt = Entry(window,width=10)
+txt.grid(column=1, row=0)
+
+chk_state = BooleanVar()
+ 
+chk_state.set(True) 
+ 
+chk = Checkbutton(window, text='Yeet?', var=chk_state)
+ 
+chk.grid(column=2, row=0)
 
 window.mainloop()     # Keep the window open
